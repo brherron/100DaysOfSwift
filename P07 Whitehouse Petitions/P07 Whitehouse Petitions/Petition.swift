f92e100d266411f9999e95ae8cbb5cdf2ce06764
Beau <beau@murraymacrecords.com>
2019-04-11 11:40:59 -0500

    Day 33 | Project 7 | Setup and Parsing JSON Data

---
+++ P07 Whitehouse Petitions/P07 Whitehouse Petitions/Petition.swift
@@ -0,0 +1,16 @@
+//
+//  Petition.swift
+//  P07 Whitehouse Petitions
+//
+//  Created by Beau Herron on 4/11/19.
+//  Copyright © 2019 Beau Herron. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+    
+}
---
+++ P07 Whitehouse Petitions/P07 Whitehouse Petitions/Petitions.swift
@@ -0,0 +1,13 @@
+//
+//  Petitions.swift
+//  P07 Whitehouse Petitions
+//
+//  Created by Beau Herron on 4/11/19.
+//  Copyright © 2019 Beau Herron. All rights reserved.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
