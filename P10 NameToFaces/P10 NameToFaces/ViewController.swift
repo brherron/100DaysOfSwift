26b270a4e7851b2f5d218262b008f89c7b1a6bb8
Beau <beau@murraymacrecords.com>
2019-04-20 20:39:36 -0500

    Day 42 | Project 10 | General Setup

---
+++ P10 NameToFaces/P10 NameToFaces/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  P10 NameToFaces
+//
+//  Created by Beau Herron on 4/20/19.
+//  Copyright © 2019 Beau Herron. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
+
+
+}
+
